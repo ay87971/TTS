541d167a4ef81a39dfe265604133467c6ba60d73
Ashutosh <your_email@example.com>
2025-01-19 03:28:09 +0530

    Updated install.py and added new files

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
-brew install git-lfs
-GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/hexgrad/Kokoro-82M
+brew install git-lfs;
+GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/hexgrad/Kokoro-82M;
 
-cd Kokoro-82M
-brew install espeak-ng
-pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
-pip install phonemizer transformers scipy munch soundfile
+cd Kokoro-82M;
+brew install espeak-ng;
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu;
+pip install phonemizer transformers scipy munch soundfile;
